493ae499545acd065766bddd3e05c9b7f70a1e5b
Paul Guilbert <guilbert.paul@gmail.com>
2019-01-21 13:45:32 +0100

    Add config & start implementing.

---
+++ src/config.h
@@ -0,0 +1,21 @@
+#ifndef Config_h
+#define Config_h
+
+/**
+ * Secret need to define the following:
+ * #define WIFI_SSID ****;
+ * #define WIFI_PASSWORD ****;
+ * */
+#include "secret.h"
+
+#define THING_UID 6fb4d7b64bef4a0c9689be18cbb216a1
+
+#define RELAY1_PIN X
+#define RELAY2_PIN X
+#define RELAY3_PIN X
+
+#define SWITCH_RELAY1_PIN Y
+#define SWITCH_RELAY2_PIN Y
+#define SWITCH_RELAY3_PIN Y
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include "config.h"
+ 
+const relay1 = Relay(RELAY1_PIN, false);
+const relay2 = Relay(RELAY2_PIN, false);
+const relay3 = Relay(RELAY3_PIN, false);
+
+const switchRelay1 = Switch(SWITCH_RELAY1_PIN, LOW);
+const switchRelay2 = Switch(SWITCH_RELAY2_PIN, LOW);
+const switchRelay3 = Switch(SWITCH_RELAY3_PIN, LOW);
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(RELAY1_PIN, OUTPUT);
+  pinMode(RELAY2_PIN, OUTPUT);
+  pinMode(RELAY3_PIN, OUTPUT);
+
+  pinMode(SWITCH_RELAY1_PIN, INPUT_PULLUP);
+  pinMode(SWITCH_RELAY2_PIN, INPUT_PULLUP);
+  pinMode(SWITCH_RELAY3_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
