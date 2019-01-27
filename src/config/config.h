7a0ca9e773259d2e5edc089555f5369d9bd99fb0
Paul Guilbert <guilbert.paul@gmail.com>
2019-01-27 11:44:29 +0100

    Add fake secret.h (for CI)

--- src/config/config.h
+++ src/config/config.h
@@ -1,11 +1,6 @@
 #ifndef Config_h
 #define Config_h
 
-/**
- * Secret need to define the following:
- * #define WIFI_SSID ****;
- * #define WIFI_PASSWORD ****;
- * */
 #include "secret.h"
 
 #define THING_UID "PG-6fb4d7b6"
---
+++ src/config/secret.h
@@ -0,0 +1,7 @@
+#ifndef Secret_h
+#define Secret_h
+
+const char* ssid = "******";
+const char* password = "*******";
+
+#endif
